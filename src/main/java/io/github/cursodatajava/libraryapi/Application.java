9a2253f737639d45b75d803e2431f6ba71cbb1b8
{DevAssis} Gabriel Assis <154300021+deveassis@users.noreply.github.com>
2026-01-09 18:38:02 -0300

    retomando os estudos

--- src/main/java/io/github/cursodatajava/libraryapi/Application.java
+++ src/main/java/io/github/cursodatajava/libraryapi/Application.java
@@ -1,6 +1,11 @@
 package io.github.cursodatajava.libraryapi;
+import io.github.cursodatajava.libraryapi.model.Autor;
+import io.github.cursodatajava.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
+import java.util.UUID;
 
 
 @SpringBootApplication
@@ -11,9 +16,18 @@ public class Application {
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-
+		var context = SpringApplication.run(Application.class, args);
+		AutorRepository autorRepository = context.getBean(AutorRepository.class);
+		exemploSalvarRegistro(autorRepository);
 	}
 
+	public static void exemploSalvarRegistro(AutorRepository autorRepository){
+		Autor autor = new Autor();
+		autor.setNome("Gabriel");
+		autor.setNacionalidade("Brasileiro");
+		autor.setDataNascimento(LocalDate.of(1997, 12, 10));
+		var autorSalvo = autorRepository.save(autor);
+		System.out.println("Autor salvo com sucesso: " + autorSalvo);
+	}
 
 }
